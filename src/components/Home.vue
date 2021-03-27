a4a9204624df1ab64c9b2769f8a7b1e66bc9cef0
Emil Madsen <emilmadsen@gmail.com>
2021-03-27 01:13:07 +0100

    init vue app. added axios, vuetify and router

---
+++ src/components/Home.vue
@@ -0,0 +1,37 @@
+<template>
+
+  <div>
+        <v-btn>Test</v-btn>
+  </div>
+
+
+</template>
+
+<script>
+
+//import axios from "axios"
+export default {
+  name: 'Home',
+  props: {
+    msg: String
+  }
+}
+
+</script>
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/plugins/vuetify.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+
+Vue.use(Vuetify)
+
+const opts = {
+    theme: {
+      themes: {
+        light: {
+          primary: '#CBAA5C',
+          secondary: '#083759',
+        },
+      },
+    },
+  }
+
+export default new Vuetify(opts)
